e0d3e7bbd2b1f7c735a35210c5c640ff39bb420a
zgregi <gregoire.weber@gmail.com>
2025-03-28 12:30:46 +0100

    début question 2

---
+++ question2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+#On extrait
+game = pd.read_csv('game.csv')
+game[ "game_date"] = pd.to_datetime(game["game_date"])
+print(game.head())
+
+# on trie les données pour avoir les matchs de saison réguliere 2022 et 2023
+debut_saison_22_23 = pd.Timestamp(2022, 10, 18)
+fin_saison_22_23 = pd.Timestamp(2023, 4, 12)
+
+game_reg_season_22_23 = game[(game['game_date'] >= debut_saison_22_23 ) & (game['game_date'] <= fin_saison_22_23)
+                             & (game["season_type"] == "Regular Season")]
+print(game_reg_season_22_23)
+
