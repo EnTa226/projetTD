7fa3d06dd174f61f390df57940d0fbbcc2673d9b
zgregi <gregoire.weber@gmail.com>
2025-05-07 14:50:01 +0200

    plus de flake 8 erreurs

--- question5.py
+++ question5.py
@@ -1,4 +1,5 @@
-# Quelle est l'équipe ayant eu le plus de joueurs qui ne sont pas arrivé en NBA par la draft ?
+# Quelle est l'équipe ayant eu le plus de joueurs qui ne sont pas
+# arrivé en NBA par la draft ?
 
 import pandas as pd
 
@@ -6,7 +7,8 @@ import pandas as pd
 df = pd.read_csv('common_player_info.csv')
 
 # On garde que les joueurs qui n'ont pas été draftés
-# On remarque que la colonne 'draft_year' contient l'année de draft ou 'Undrafted'
+# On remarque que la colonne 'draft_year' contient l'année
+# de draft ou 'Undrafted'
 undrafted_players = df[df['draft_year'] == 'Undrafted']
 
 # On compte le nombre de joueurs "Undrafted" par équipe ( colone team_name)
@@ -16,4 +18,4 @@ undrafted_counts = undrafted_players['team_name'].value_counts()
 # Afficher les résultats
 print(undrafted_counts[0:1])
 
-#Il s'agit donc de l'équipe des Hawks
+# Il s'agit donc de l'équipe des Hawks
